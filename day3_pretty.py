ae29767aab17815d9b37d1f6ffa1ec7f23cb6cce
Arrain <ser_arrain@yahoo.com>
2021-12-02 23:30:10 -0700

    Remove extra loops from day 3

--- day3_pretty.py
+++ day3_pretty.py
@@ -20,35 +20,33 @@ print("*********")
 
 workInput = input.copy()
 
-while(len(workInput) > 1):
-    for p in range(lineSize):
-        freq0 = len(list(filter(lambda x: x[p] == "0", workInput)))
-        freq1 = len(list(filter(lambda x: x[p] == "1", workInput)))
+for p in range(lineSize):
+    freq0 = len(list(filter(lambda x: x[p] == "0", workInput)))
+    freq1 = len(list(filter(lambda x: x[p] == "1", workInput)))
 
-        c = "0" if freq0 > freq1 else "1"
+    c = "0" if freq0 > freq1 else "1"
 
-        workInput = list(filter(lambda x: x[p] == c, workInput))
+    workInput = list(filter(lambda x: x[p] == c, workInput))
 
-        # check if done before all positions are processed
-        if(len(workInput) == 1):
-            break
+    # check if done before all positions are processed
+    if(len(workInput) == 1):
+        break
 
 oxigen = int(workInput[0], 2)
 
 workInput = input.copy()
 
-while(len(workInput) > 1):
-    for p in range(lineSize-1):
-        freq0 = len(list(filter(lambda x: x[p] == "0", workInput)))
-        freq1 = len(list(filter(lambda x: x[p] == "1", workInput)))
+for p in range(lineSize-1):
+    freq0 = len(list(filter(lambda x: x[p] == "0", workInput)))
+    freq1 = len(list(filter(lambda x: x[p] == "1", workInput)))
 
-        c = "1" if freq1 < freq0 else "0"
+    c = "1" if freq1 < freq0 else "0"
 
-        workInput = list(filter(lambda x: x[p] == c, workInput))
+    workInput = list(filter(lambda x: x[p] == c, workInput))
 
-        # check if done before all positions are processed
-        if(len(workInput) == 1):
-            break
+    # check if done before all positions are processed
+    if(len(workInput) == 1):
+        break
 
 co2 = int(workInput[0], 2)
 
