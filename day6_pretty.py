512400047ae8cbc3c3e323fb8a95e6c1cd03a443
Arrain <ser_arrain@yahoo.com>
2021-12-05 23:04:51 -0700

    Smoothen day 6

---
+++ day6_pretty.py
@@ -0,0 +1,33 @@
+
+input = []
+
+with open("day6_input.txt") as file:
+    input = [list(map(lambda i: int(i), line.split(",")))
+             for line in file.readlines()]
+
+fish = input[0]
+for i in range(80):
+    newFish = []
+    for f in range(len(fish)):
+        if(fish[f] == 0):
+            newFish.append(8)
+            fish[f] = 6
+        else:
+            fish[f] -= 1
+
+    fish.extend(newFish)
+
+print(len(fish))
+
+
+gFish = list(map(lambda: 0, range(9)))
+for i in input[0]:
+    gFish[i] += 1
+for i in range(256):
+    newFish = gFish[0]
+    for f in range(1, 9):
+        gFish[f-1] = gFish[f]
+    gFish[6] += newFish
+    gFish[8] = newFish
+
+print(sum(gFish))
