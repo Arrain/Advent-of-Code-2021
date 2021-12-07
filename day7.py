cf305b8d302fb87f31d30faf4f4d7c993d2ade0a
Arrain <ser_arrain@yahoo.com>
2021-12-06 22:24:26 -0700

    Day 7

---
+++ day7.py
@@ -0,0 +1,21 @@
+import sys
+input = []
+
+with open("day7_input.txt") as file:
+    input = [line
+             for line in file.readlines()]
+
+crabs = list(map(lambda x: int(x), input[0].split(",")))
+
+cMax = max(crabs)
+cMin = min(crabs)
+
+eMin = sys.maxsize
+lvl = -1
+for i in range(cMin, cMax + 1):
+    e = sum(map(lambda x: (abs(i - x) * (abs(i - x) + 1)) / 2, crabs))
+    if(e < eMin):
+        lvl = i
+        eMin = e
+
+print(eMin, lvl)
